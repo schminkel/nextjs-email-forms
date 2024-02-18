9f12a63a3e12bcc2d8110a47238572edf05520c6
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-18 14:54:57 +0100

    fix reserved env

--- src/lib/aws-ses-email.js
+++ src/lib/aws-ses-email.js
@@ -1,7 +1,7 @@
 import { SES } from '@aws-sdk/client-ses';
 
 const ses = new SES( {
-  accessKeyId: process.env.AWS_ACCESS_KEY || "",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   region: process.env.AWS_REGION || ""
 } )
